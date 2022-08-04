5ab653b773eed053fa8fd141c85a22ef0d150e64
anoorishad <anoorishad@me.com>
2022-08-03 18:14:19 -0700

    Added question details

--- lc.js
+++ lc.js
@@ -1,1 +1,9 @@
-// Start file
+// Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var validPalindrome = function(s) {
+    
+};
+
